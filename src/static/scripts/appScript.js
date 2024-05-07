6242e158bbb71622f9a8a349ab16b730ef1e4ba6
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-06 23:23:14 -0700

    Elementos ya interactuan con la página

--- src/static/scripts/appScript.js
+++ src/static/scripts/appScript.js
@@ -1,6 +1,30 @@
 const input = document.getElementById("in");
 const output = document.getElementById("out");
 const button = document.getElementById("anBtn");
+const t1 = document.getElementById("t1");
+const t2 = document.getElementById("t2");
+
+var actualSt = 1;
+
+function changeColor(st) {
+  if (st === 1) {
+    t1.style.backgroundColor = "#0a255f";
+    t2.style.backgroundColor = "#0d47a1";
+  } else {
+    t2.style.backgroundColor = "#0a255f";
+    t1.style.backgroundColor = "#0d47a1";
+  }
+}
+
+t1.addEventListener("click", () => {
+  actualSt = 1;
+  changeColor(actualSt);
+});
+
+t2.addEventListener("click", () => {
+  actualSt = 2;
+  changeColor(actualSt);
+});
 
 button.addEventListener("click", () => {
   let cadena = {
@@ -12,8 +36,12 @@ button.addEventListener("click", () => {
       output.value = req.responseText;
     }
   };
-  req.open("POST", "analizar", true);
+  actualSt === 1
+    ? req.open("POST", "lexer", true)
+    : req.open("POST", "parser", true);
   req.setRequestHeader("Content-type", "application/json");
   req.send(JSON.stringify(cadena));
   console.log(cadena);
 });
+
+changeColor(actualSt);
