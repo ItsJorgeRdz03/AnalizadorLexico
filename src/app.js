f688dbe8af202fc6ed839543503d5e46cc42d1ed
ItsJorgeRdz03 <82615294+ItsJorgeRdz03@users.noreply.github.com>
2024-05-06 23:25:27 -0700

    Conectado el parser a la con la página

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import { fileURLToPath } from "url";
 import path from "path";
 import * as lexer from "./lexer.js";
+import * as parser from "./parser.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,12 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", async (req, res) => {
-  //res.send(html);
   res.sendFile("/static/index.html");
 });
 
-app.post("/analizar", async (req, res) => {
+app.post("/lexer", async (req, res) => {
   let data = await lexer.analizar(req.body.string);
+  console.log(data);
+  res.send(data).end;
+  data = "";
+});
+
+app.post("/parser", async (req, res) => {
+  if (req.body.string === "") {
+    res.send("").end;
+  }
+  let data = await parser.analizar(req.body.string);
   console.log(data);
   res.send(data).end;
   data = "";
